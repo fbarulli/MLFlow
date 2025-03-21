efc462fc6338b564f033bda52d849c5c0a64ed09
Ubuntu <ubuntu@ip-172-31-31-103.eu-west-1.compute.internal>
2025-03-21 14:33:49 +0000

    starting get_data function

---
+++ get_data.py
@@ -0,0 +1,45 @@
+# get_data.py
+import requests
+import pandas as pd 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime
+import json
+import os
+from typing import List, Dict, Optional, Any
+import traceback
+import csv
+import logging
+import sys
+from pydantic import BaseModel, ValidationError
+from datetime import timedelta
+
+
+
+
+
+
+CITIES = ["Paris", "London", "New York", "Berlin", "Tokyo"]
+COUNTRIES = ["FR", "GB", "US", "DE", "JP"]
+API_URL = "https://api.openweathermap.org/data/2.5/weather"
+API_KEY = "fcf2e035cc53b836623ac91cccd8848d" 
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+logger.addHandler(console_handler)
+
+
+class WeatherData(BaseModel):
+    city : str
+    country : str
+    main_temp : float
+    main_pressure : int
+    main_humidity : int
+
+def log_to_csv(function_name: str,
+               message : str,
+               status : str) ->None:
+    '''Logs 
